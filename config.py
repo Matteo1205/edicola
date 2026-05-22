07513dab83055cdbd7dc9b4be08bd6000b4c43ce
Matteo <matteoferlotta@gmail.com>
2026-05-22 14:39:36 +0200

    creazione primo html e fix config file

--- config.py
+++ config.py
@@ -2,9 +2,7 @@ import os
 from dotenv import load_dotenv, find_dotenv
 
 # Carica le variabili d'ambiente dal file .env se esiste
-_dotenv_path = find_dotenv()
-if _dotenv_path:
-    load_dotenv(_dotenv_path)
+load_dotenv()
 
 
 # Configurazione dell'applicazione Flask e del database
@@ -17,8 +15,8 @@ class Config:
     DATABASE_NAME = os.environ.get('DATABASE_NAME', '')
     DATABASE_USER = os.environ.get('DATABASE_USER', '')
     DATABASE_PASSWORD = os.environ.get('DATABASE_PASSWORD', '')
-    FLASK_DEBUG = os.environ.get('FLASK_DEBUG', '')
-
+    FLASK_DEBUG = os.environ.get('FLASK_DEBUG', 'false').lower() in ('1', 'true', 'yes')
+    
     DB_PARAMS = {
         'host': DATABASE_HOST,
         'port': DATABASE_PORT,
